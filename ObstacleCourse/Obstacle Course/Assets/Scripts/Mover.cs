eea7228adefdf5a9ddd135fa8f4dcffcd1867147
Jon <66325051+ShukaYou@users.noreply.github.com>
2022-05-22 21:36:44 -0400

    player movement
    added scripts and basic playe4r movement

--- ObstacleCourse/Obstacle Course/Assets/Scripts/Mover.cs
+++ ObstacleCourse/Obstacle Course/Assets/Scripts/Mover.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Variables that affect movement speed                     
+    [SerializeField] float movementSpeed = 10.0f;
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime *movementSpeed;
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime *movementSpeed;
+        transform.Translate(xValue,0,zValue);
     }
 }
